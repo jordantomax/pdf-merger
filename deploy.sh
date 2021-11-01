0c2b9e364c57904b21e066240ef00e301a23d0d3
Jordan Cooperman <jordancooperman@gmail.com>
2021-11-01 15:44:47 +0800

    First draft of pdf merger with deploy script

---
+++ deploy.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+function check_ok () {
+  if [ $? -eq 0 ]
+  then
+    echo "OK"
+  else
+    echo "ERROR" && exit 1
+  fi
+}
+
+function zip_lambda ()
+{
+  echo "Zipping dependencies"
+  cd venv/lib/python3.9/site-packages
+  zip -r ../../../../deployment_package.zip .
+  cd ../../../../
+
+  echo "Zipping lambda"
+  zip -g deployment_package.zip handler.py
+  check_ok
+}
+
+function sync_lambda ()
+{
+  echo "syncing lambda"
+  aws --region us-east-1 lambda update-function-code --function-name PdfMerger --zip-file fileb://./deployment_package.zip --profile greenmanatee
+  check_ok
+}
+
+function delete_zip ()
+{
+  echo "deleting zip"
+  rm deployment_package.zip
+  check_ok
+}
+
+
+zip_lambda
+sync_lambda
+delete_zip
+exit 0
---
+++ handler.py
@@ -0,0 +1,35 @@
+from PyPDF2 import PdfFileMerger, PdfFileReader
+from urllib.request import urlopen
+from io import BytesIO
+
+import base64
+import json
+
+def handler(event, context):
+    body = json.loads(event.get('body'))
+    pdf_urls = body.get('pdf_urls')
+    if not pdf_urls:
+        return {
+            'statusCode': 400,
+            'body': 'Malformed body. Body must consist of a json object containing a key "pdf_urls" with value as an array of strings'
+        }
+
+    pdf_merged = PdfFileMerger()
+
+    for url in pdf_urls:
+        pdf_file = urlopen(url).read()
+        memory_file = BytesIO(pdf_file)
+        pdf_merged.append(PdfFileReader(memory_file))
+
+    pdf_merged.write('/tmp/merged.pdf')
+
+    return {
+        'statusCode': 200,
+        'headers': {
+            'Content-Type': 'application/pdf',
+            'Access-Control-Allow-Headers': 'Content-Type',
+            'Access-Control-Allow-Origin': 'https://shippo.lazycoconuts.com'
+        },
+        'body': base64.b64encode(open('/tmp/merged.pdf', 'rb').read()).decode('utf-8'),
+        'isBase64Encoded': True
+    }
